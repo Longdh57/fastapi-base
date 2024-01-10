aa5c0982612c2b96ef17c15fc8d3dded3191b92d
ducanh-99 <ducanhhust99@gmail.com>
2024-01-10 15:52:47 +0700

    Feature: change API layer dependences service layer

--- app/api/api_register.py
+++ app/api/api_register.py
@@ -1,10 +1,8 @@
 from typing import Any
 
 from fastapi import APIRouter, Depends
-from fastapi_sqlalchemy import db
 
 from app.helpers.exception_handler import CustomException
-from app.models import User
 from app.schemas.sche_base import DataResponse
 from app.schemas.sche_user import UserItemResponse, UserRegisterRequest
 from app.services.srv_user import UserService
@@ -13,12 +11,9 @@ router = APIRouter()
 
 
 @router.post('', response_model=DataResponse[UserItemResponse])
-def register(register_data: UserRegisterRequest) -> Any:
+def register(register_data: UserRegisterRequest, user_service: UserService = Depends()) -> Any:
     try:
-        exist_user = db.session.query(User).filter(User.email == register_data.email).first()
-        if exist_user:
-            raise Exception('Email already exists')
-        register_user = UserService().register_user(register_data)
+        register_user = user_service.register_user(register_data)
         return DataResponse().success_response(data=register_user)
     except Exception as e:
         raise CustomException(http_code=400, code='400', message=str(e))
